4cf537dffd45f606d483ff984845da494872fb74
TomMonks <t.m.w.monks@exeter.ac.uk>
2026-02-12 15:55:55 +0000

    feat(dp): python use case examples

---
+++ pages/code/defensive_programming__wrong_datatype.py
@@ -0,0 +1,6 @@
+patient_data = [
+    {'ARRIVAL_DATE': '2026-01-15', 'ARRIVAL_TIME': '0900'},
+    {'ARRIVAL_DATE': '2026-01-15', 'ARRIVAL_TIME': '1030'}
+]
+
+result = calculate_wait_times(patient_data)
